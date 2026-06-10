8fbc2cad6f047150deee3cf585f7a0d059223798
Mihir-Arjun-dvt <MArjun@dvtsoftware.com>
2026-06-10 14:47:23 +0200

    test: elevator direction is Up when moving to higher floor

--- ElevatorSim.Tests/UnitTest1.cs
+++ ElevatorSim.Tests/UnitTest1.cs
@@ -1,10 +1,12 @@
 ﻿namespace ElevatorSim.Tests;
 
-public class UnitTest1
+public class DomainUnitTests
 {
     [Fact]
-    public void Test1()
+    public void MoveToFloor_SetsDirectionUp_WhenTargetIsHigher()
     {
-
+        var elevator = new PassengerElevator(capacity: 10, startFloor: 1);
+        elevator.MoveToFloor(5);
+        Assert.Equal(ElevatorDirection.Up, elevator.Direction);
     }
 }
