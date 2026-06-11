ae8e0b712c5ed530e9f74131c43ea077fecc092d
Mihir-Arjun-dvt <MArjun@dvtsoftware.com>
2026-06-11 10:30:29 +0200

    feat: implemented the elevator base class

---
+++ ElevatorSim.Domain/ElevatorBase.cs
@@ -0,0 +1,62 @@
+public class ElevatorBase : IElevator
+{
+    private int _currentFloor { get; set; }
+    private ElevatorDirection _direction { get; set; }
+    private ElevatorState _state { get; set; }
+    private int _passengerCount { get; set; }
+
+    public int CurrentFloor => _currentFloor;
+    public ElevatorDirection Direction => _direction;
+    public ElevatorState State => _state;
+    public int PassengerCount => _passengerCount;
+    public bool CanAcceptPassengers => _passengerCount < Capacity;
+    public int Capacity { get; }
+
+    protected ElevatorBase(int capacity, int startFloor = 1)
+    {
+        Capacity = capacity;
+        _currentFloor = startFloor;
+        _direction = ElevatorDirection.Stationary;
+        _state = ElevatorState.Idle;
+        _passengerCount = 0;
+    }
+
+    public virtual void MoveToFloor(int floor)
+    {
+        if (_currentFloor == floor)
+        {
+            _direction = ElevatorDirection.Stationary;
+            return;
+        }
+        if (floor > _currentFloor)
+        {
+            _direction = ElevatorDirection.Up;
+        }
+        else if (floor < _currentFloor)
+        {
+            _direction = ElevatorDirection.Down;
+        }
+        _currentFloor = floor;
+        _state = ElevatorState.Idle;
+    }
+
+    public void AddPassengers(int count)
+    {
+        if (_passengerCount + count > Capacity)
+        {
+            // @TODO: throw custome exception for capacity being exceeded
+        }
+
+        _passengerCount += count;
+    }
+
+    public void RemovePassengers(int count)
+    {
+        if (count > _passengerCount)
+        {
+            throw new ArgumentOutOfRangeException(nameof(count), $"Cannot remove {count} passengers, only {_passengerCount} onboard.");
+        }
+
+        _passengerCount -= count;
+    }
+}
--- ElevatorSim.Domain/Interfaces/IElevator.cs
+++ ElevatorSim.Domain/Interfaces/IElevator.cs
@@ -1,11 +1,11 @@
 public interface IElevator
 {
-    int CurrentFloor { get; set; }
-    ElevatorDirection Direction { get; set; }
-    ElevatorState State { get; set; }
-    int PassengerCount { get; set; }
-    int Capacity { get; set; }
-    bool CanAcceptPassengers { get; set; }
+    int CurrentFloor { get; }
+    ElevatorDirection Direction { get; }
+    ElevatorState State { get; }
+    int PassengerCount { get; }
+    int Capacity { get; }
+    bool CanAcceptPassengers { get; }
 
     void MoveToFloor(int floor);
     void AddPassengers(int count);
