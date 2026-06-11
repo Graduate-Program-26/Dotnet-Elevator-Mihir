542e3e63f1865ac0248125da5c95d22aa47e3856
Mihir-Arjun-dvt <MArjun@dvtsoftware.com>
2026-06-11 18:40:47 +0200

    tests: added tests for the freight elevator implementation

--- ElevatorSim.Tests/ApplicationLayerTests.cs
+++ ElevatorSim.Tests/ApplicationLayerTests.cs
@@ -10,5 +10,21 @@ public class ApplicationLayerTests
         Assert.Equal(PassengerElevator.DefaultCapacity, elevator.Capacity);
     }
 
+    [Fact]
+    public void FreightElevator_HasHigherDefaultCapacity_ThanPassengerElevator()
+    {
+        var elevator = new FreightElevator();
 
+        Assert.True(elevator.Capacity > PassengerElevator.DefaultCapacity);
+    }
+
+    [Fact]
+    public void FreightElevator_CanBeUsedAs_IElevator()
+    {
+        IElevator elevator = new FreightElevator();
+
+        elevator.MoveToFloor(3);
+
+        Assert.Equal(3, elevator.CurrentFloor);
+    }
 }
