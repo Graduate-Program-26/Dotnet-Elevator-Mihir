04d37a2bfda1ce64649a968e732d80e680734be3
Mihir-Arjun-dvt <MArjun@dvtsoftware.com>
2026-06-11 18:42:59 +0200

    feat: added a FreightElevator implementation

--- ElevatorSim.Application/Elevators/FreightElevator.cs
+++ ElevatorSim.Application/Elevators/FreightElevator.cs
@@ -1,4 +1,7 @@
 public class FreightElevator : ElevatorBase
 {
+    public const int DefaultCapacity = 20;
 
+    public FreightElevator(int capacity = DefaultCapacity, int startFloor = 1)
+        : base(capacity, startFloor) { }
 }
