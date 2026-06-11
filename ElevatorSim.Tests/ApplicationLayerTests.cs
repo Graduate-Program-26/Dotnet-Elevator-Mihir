cae8e8dc2f284d3cd03a147f6a85576d473f3097
Mihir-Arjun-dvt <MArjun@dvtsoftware.com>
2026-06-11 18:49:20 +0200

    tests: tests added for high speed elevator

--- ElevatorSim.Tests/ApplicationLayerTests.cs
+++ ElevatorSim.Tests/ApplicationLayerTests.cs
@@ -27,4 +27,33 @@ public class ApplicationLayerTests
 
         Assert.Equal(3, elevator.CurrentFloor);
     }
+
+    [Fact]
+    public void HighSpeedElevator_HasLowerDefaultCapacity_ThanPassengerElevator()
+    {
+        var elevator = new HighSpeedElevator();
+
+        Assert.True(elevator.Capacity < PassengerElevator.DefaultCapacity);
+    }
+
+    [Fact]
+    public void HighSpeedElevator_MoveToFloor_SetsStateToMoving_ThenIdle()
+    {
+        var elevator = new HighSpeedElevator();
+
+        elevator.MoveToFloor(10);
+
+        Assert.Equal(10, elevator.CurrentFloor);
+        Assert.Equal(ElevatorState.Idle, elevator.State);
+    }
+
+    [Fact]
+    public void HighSpeedElevator_CanBeUsedAs_IElevator()
+    {
+        IElevator elevator = new HighSpeedElevator();
+
+        elevator.MoveToFloor(5);
+
+        Assert.Equal(5, elevator.CurrentFloor);
+    }
 }
