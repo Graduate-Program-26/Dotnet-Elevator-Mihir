29b180649bea29c00840554d10478138897fa5b4
Mihir-Arjun-dvt <MArjun@dvtsoftware.com>
2026-06-12 16:01:22 +0200

    feat: wired up everything for testing purposes

--- ElevatorSim.Console/Program.cs
+++ ElevatorSim.Console/Program.cs
@@ -1,1 +1,40 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+using Serilog;
+
+var config = new SimulationConfig(
+    NumberOfFloors: 20,
+    NumberOfElevators: 3,
+    ElevatorCapacity: 10);
+
+var services = new ServiceCollection()
+    .AddLogging(logging =>
+    {
+        logging.ClearProviders();
+        logging.AddSerilog(new LoggerConfiguration()
+            .WriteTo.Console()
+            .CreateLogger());
+    })
+    .AddElevatorSimulation(config)
+    .BuildServiceProvider();
+
+var controller = services.GetRequiredService<IElevatorController>();
+
+var statuses = controller.GetStatuses();
+
+Console.WriteLine($"Simulation started with {statuses.Count()} elevators:");
+Console.WriteLine();
+
+foreach (var (status, index) in statuses.Select((s, i) => (s, i)))
+{
+    Console.WriteLine($"Elevator #{index + 1}");
+    Console.WriteLine($"  Floor:      {status.CurrentFloor}");
+    Console.WriteLine($"  Direction:  {status.Direction}");
+    Console.WriteLine($"  State:      {status.State}");
+    Console.WriteLine($"  Passengers: {status.PassengerCount} / {status.Capacity}");
+    Console.WriteLine();
+}
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
