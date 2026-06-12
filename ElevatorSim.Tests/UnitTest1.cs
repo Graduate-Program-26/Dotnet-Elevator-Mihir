b08a88ed06090122ecb83a8363c012f4d30bd949
Mihir-Arjun-dvt <MArjun@dvtsoftware.com>
2026-06-12 10:09:58 +0200

    refactor: updated old tests to work with new functionality

--- ElevatorSim.Tests/UnitTest1.cs
+++ ElevatorSim.Tests/UnitTest1.cs
@@ -25,20 +25,32 @@ public class DomainUnitTests
     [Fact]
     public void RequestElevator_UsesDispatchStrategy_ToSelectElevator()
     {
+        var mockElevator = new Mock<IElevator>();
+        mockElevator.Setup(e => e.Capacity).Returns(10);
+        mockElevator.Setup(e => e.PassengerCount).Returns(0);
+        mockElevator.Setup(e => e.CanAcceptPassengers).Returns(true);
+        mockElevator.Setup(e => e.State).Returns(ElevatorState.Idle);
+
         var mockStrategy = new Mock<IDispatchStrategy>();
-        var controller = new ElevatorController(elevators, mockStrategy.Object);
+        mockStrategy
+            .Setup(s => s.SelectElevator(
+                It.IsAny<IEnumerable<IElevator>>(), 5, 2))
+            .Returns(mockElevator.Object);
+
+        var controller = new ElevatorController([mockElevator.Object], mockStrategy.Object);
 
         controller.RequestElevator(5, 2);
 
         mockStrategy.Verify(s => s.SelectElevator(
-            It.IsAny<IEnumerable<IElevator>>(), 5, 2), Timer.Once);
+            It.IsAny<IEnumerable<IElevator>>(), 5, 2), Times.Once);
     }
 
     [Fact]
     public void GetStatuses_ReturnsStatusForAllElevators()
     {
         var elevator = new PassengerElevator(capacity: 10, startFloor: 1);
-        var controller = new ElevatorController([elevator]);
+        var mockStrategy = new Mock<IDispatchStrategy>();
+        var controller = new ElevatorController([elevator], mockStrategy.Object);
 
         var statuses = controller.GetStatuses();
 
@@ -46,5 +58,7 @@ public class DomainUnitTests
         Assert.Equal(1, statuses.First().CurrentFloor);
         Assert.Equal(ElevatorDirection.Stationary, statuses.First().Direction);
         Assert.Equal(ElevatorState.Idle, statuses.First().State);
+        Assert.Equal(0, statuses.First().PassengerCount);
+        Assert.Equal(10, statuses.First().Capacity);
     }
 }
