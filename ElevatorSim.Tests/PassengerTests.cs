d572e7cdb41be4e5d3e4135edebd7215d3f1c338
Mihir-Arjun-dvt <MArjun@dvtsoftware.com>
2026-06-14 12:27:30 +0200

    feat: added an initial passenger test

---
+++ ElevatorSim.Tests/PassengerTests.cs
@@ -0,0 +1,12 @@
+namespace ElevatorSim.Tests;
+
+public class PassengerTests
+{
+    [Fact]
+    public void Passenger_HasStartAndDestinationFloor()
+    {
+        var passenger = new Passenger(startingFloor: 1, destinationFloor: 5);
+        Assert.Equal(1, passenger.StartingFloor);
+        Assert.Equal(5, passenger.DestinationFloor);
+    }
+}
