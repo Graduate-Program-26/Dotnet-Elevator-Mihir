2c8a05b8c7223a970688f04cd8462fbfe61b4328
Mihir-Arjun-dvt <MArjun@dvtsoftware.com>
2026-06-14 12:38:25 +0200

    tests: added further tests for passengers

--- ElevatorSim.Tests/PassengerTests.cs
+++ ElevatorSim.Tests/PassengerTests.cs
@@ -5,8 +5,32 @@ public class PassengerTests
     [Fact]
     public void Passenger_HasStartingAndDestinationFloor()
     {
-        var passenger = new Passenger(startingFloor: 1, destinationFloor: 5);
+        var passenger = new Passenger(StartingFloor: 1, DestinationFloor: 5);
+
         Assert.Equal(1, passenger.StartingFloor);
         Assert.Equal(5, passenger.DestinationFloor);
+    }
+
+    [Fact]
+    public void BoardPassenger_AddsPassengerWithDestination()
+    {
+        var elevator = new PassengerElevator(capacity: 10, startFloor: 1);
+        var passenger = new Passenger(StartingFloor: 1, DestinationFloor: 5);
+
+        elevator.BoardPassenger(passenger);
+
+        Assert.Equal(1, elevator.PassengerCount);
+    }
+
+    [Fact]
+    public void BoardPassenger_TracksPassengerDestination()
+    {
+
+        var elevator = new PassengerElevator(capacity: 10, startFloor: 1);
+        var passenger = new Passenger(StartingFloor: 1, DestinationFloor: 5);
+
+        elevator.BoardPassenger(passenger);
+
+        Assert.Contains(5, elevator.DestinationFloors);
     }
 }
