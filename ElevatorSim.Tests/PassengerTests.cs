8949b7b6b553e6d536a4602645d2e2957b8851cc
Mihir-Arjun-dvt <MArjun@dvtsoftware.com>
2026-06-14 12:30:22 +0200

    feat: added a basic passenger record

--- ElevatorSim.Tests/PassengerTests.cs
+++ ElevatorSim.Tests/PassengerTests.cs
@@ -3,7 +3,7 @@ namespace ElevatorSim.Tests;
 public class PassengerTests
 {
     [Fact]
-    public void Passenger_HasStartAndDestinationFloor()
+    public void Passenger_HasStartingAndDestinationFloor()
     {
         var passenger = new Passenger(startingFloor: 1, destinationFloor: 5);
         Assert.Equal(1, passenger.StartingFloor);
