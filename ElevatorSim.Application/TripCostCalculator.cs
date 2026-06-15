c01c51e797837efedbd666233ae1157e5028e827
Mihir-Arjun-dvt <MArjun@dvtsoftware.com>
2026-06-15 10:51:30 +0200

    refactor: updated trip calculator to penalise expensive direction changes

--- ElevatorSim.Application/TripCostCalculator.cs
+++ ElevatorSim.Application/TripCostCalculator.cs
@@ -3,7 +3,6 @@ public static class TripCostCalculator
     public static double Calculate(IElevator elevator, int startingFloor, IEnumerable<Passenger> passengers)
     {
         var passengerList = passengers.ToList();
-
         if (passengerList.Count == 0)
         {
             return 0;
@@ -24,6 +23,30 @@ public static class TripCostCalculator
             currentFloor = destination;
         }
 
-        return (double)totalDistance / elevator.Speed;
+        const int directionChangePenalty = 5;
+        var penalty = CountDirectionChanges(elevator.CurrentFloor, startingFloor, destinations) * directionChangePenalty;
+
+        return ((double)totalDistance / elevator.Speed) + penalty;
+    }
+
+    private static int CountDirectionChanges(int startFloor, int originFloor, IEnumerable<int> destinations)
+    {
+        var allStops = new[] { startFloor, originFloor }
+            .Concat(destinations)
+            .ToList();
+
+        var changes = 0;
+        for (int i = 1; i < allStops.Count - 1; i++)
+        {
+            var incoming = allStops[i] - allStops[i - 1];
+            var outgoing = allStops[i + 1] - allStops[i];
+
+            if (incoming != 0 && outgoing != 0 &&
+                Math.Sign(incoming) != Math.Sign(outgoing))
+            {
+                changes++;
+            }
+        }
+        return changes;
     }
 }
