2834d5ce342a3a02b30395688df854b6c3de9072
Mihir-Arjun-dvt <MArjun@dvtsoftware.com>
2026-06-15 09:56:54 +0200

    refactor: added speed to passenger elevator class

--- ElevatorSim.Application/Elevators/PassengerElevator.cs
+++ ElevatorSim.Application/Elevators/PassengerElevator.cs
@@ -1,4 +1,7 @@
-public class PassengerElevator(int capacity = PassengerElevator.DefaultCapacity, int startFloor = 1) : ElevatorBase(capacity, startFloor)
+public class PassengerElevator : ElevatorBase
 {
     public const int DefaultCapacity = 10;
+    public const int DefaultSpeed = 1;
+
+    public PassengerElevator(int capacity = DefaultCapacity, int startFloor = 1) : base(capacity, DefaultSpeed, startFloor) { }
 }
