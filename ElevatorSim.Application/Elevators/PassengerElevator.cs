17b0cc02ca77e083b150d5323bbf4c64abbc8049
Mihir-Arjun-dvt <MArjun@dvtsoftware.com>
2026-06-18 10:17:25 +0200

    feat: added XML comments to the elevators

--- ElevatorSim.Application/Elevators/PassengerElevator.cs
+++ ElevatorSim.Application/Elevators/PassengerElevator.cs
@@ -2,6 +2,11 @@ using ElevatorSim.Domain.Base;
 
 namespace ElevatorSim.Application.Elevators;
 
+/// <summary>
+/// A concrete implementation of an elevator designed for passenger transport.
+/// </summary>
+/// <param name="capacity">The maximum number of passengers the elevator can carry.</param>
+/// <param name="startFloor">The floor where the elevator starts.</param>
 public class PassengerElevator(int capacity = PassengerElevator.DefaultCapacity, int startFloor = 1) : ElevatorBase(capacity, DefaultSpeed, startFloor)
 {
     public const int DefaultCapacity = 10;
