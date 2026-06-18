047b0c697da88ccb42805874165a8c3c287391c3
Mihir-Arjun-dvt <MArjun@dvtsoftware.com>
2026-06-18 09:51:34 +0200

    feat: added XML comments to the domain models

--- ElevatorSim.Domain/Models/ElevatorDirection.cs
+++ ElevatorSim.Domain/Models/ElevatorDirection.cs
@@ -1,3 +1,6 @@
 namespace ElevatorSim.Domain.Models;
 
+/// <summary>
+/// Represents the direction an elevator is moving. The directions include: Stationary, Up and Down.
+/// </summary>
 public enum ElevatorDirection { Stationary, Up, Down }
--- ElevatorSim.Domain/Models/ElevatorState.cs
+++ ElevatorSim.Domain/Models/ElevatorState.cs
@@ -1,3 +1,6 @@
 namespace ElevatorSim.Domain.Models;
 
+/// <summary>
+/// Represents the current state of an elevator. The states include: Idle, Moving and DoorsOpen.
+/// </summary>
 public enum ElevatorState { Idle, Moving, DoorsOpen }
